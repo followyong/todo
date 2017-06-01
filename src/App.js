80f62e6b08e08b0abfd277b39e443dc674b47cec
followyong <947690826@qq.com>
2017-06-01 14:12:15 +0800

    hello

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Main from './Main'
+import { Provider } from 'react-redux'
+import store from './redux/store'
+
+class App extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Main />
+      </Provider>
+    );
+  }
+}
+
+export default App;
